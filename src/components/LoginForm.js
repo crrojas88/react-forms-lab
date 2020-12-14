4f0a26a2feb573a4977db9f0686823ebb1d73ec4
crrojas88 <carlorrojas@gmail.com>
2020-12-14 15:32:00 -0800

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,42 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    if (this.state.username.length > 0 && this.state.password.length > 0) {
+      let loginData = {
+        username: this.state.username,
+        password: this.state.password
+      }
+      this.props.handleLogin(loginData)
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input onChange={this.handleChange} value={this.state.username} id="username" name="username" type="text" />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input onChange={this.handleChange} value={this.state.password} id="password" name="password" type="password" />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,25 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ''
+    };
   }
+
+  handleChange = (e) => {
+    this.setState({
+      message: e.target.value
+    })
+  }
+
+
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input onChange={this.handleChange} value={this.state.message} type="text" name="message" id="message" />
+        <h6>Characters Remaining: {this.props.maxChars - this.state.message.length}</h6>
       </div>
     );
   }
